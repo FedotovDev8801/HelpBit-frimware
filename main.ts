99fdd9075389ee4269018c7240564e1167b0bce7
Daniel Fedotov <dfeed88@proton.me>
2025-01-16 16:08:44 +0100

    Some features added.

--- main.ts
+++ main.ts
@@ -11,7 +11,21 @@ makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 45)
 })
 makerbit.onIrButton(IrButton.Ok, IrButtonAction.Pressed, function () {
-	
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # . # # .
+        # . # # #
+        . # # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
 })
 basic.showLeds(`
     . . . . #
