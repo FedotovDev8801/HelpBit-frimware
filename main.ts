70fad1e8ea75bb2fa369a5627606b4939a3aa97d
Daniel Fedotov <dfeed88@proton.me>
2025-01-17 17:43:22 +0100

    Added A+B, A and B keys features and SOS signal send.

--- main.ts
+++ main.ts
@@ -2,9 +2,71 @@ enum RadioMessage {
     message1 = 49434,
     sosSignal = 40523
 }
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.sendMessage(RadioMessage.sosSignal)
+    radio.sendString("Help! SOS!")
+    music.play(music.stringPlayable("C D C D C D C D ", 360), music.PlaybackMode.UntilDone)
+})
 radio.onReceivedMessage(RadioMessage.sosSignal, function () {
     music.play(music.stringPlayable("C5 C C5 C C5 C C5 C ", 360), music.PlaybackMode.InBackground)
     basic.showString("I recieved a SOS signal! Someone needs help!")
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
 })
 makerbit.onIrButton(IrButton.Up, IrButtonAction.Released, function () {
     maqueenPlusV2.controlMotorStop(maqueenPlusV2.MyEnumMotor.AllMotor)
@@ -17,6 +79,33 @@ makerbit.onIrButton(IrButton.Down, IrButtonAction.Released, function () {
 })
 makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
     maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, 45)
+})
+input.onButtonPressed(Button.AB, function () {
+    music.play(music.stringPlayable("B C5 - - - - - - ", 450), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # #
+        # . . # #
+        # . . . .
+        # . . # .
+        . # # . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
 })
 makerbit.onIrButton(IrButton.Ok, IrButtonAction.Pressed, function () {
     basic.showLeds(`
@@ -149,9 +238,6 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `)
-basic.forever(function () {
-	
-})
 basic.forever(function () {
     datalogger.log(datalogger.createCV("startingSeconds", 1))
     secondsFromStart += 1
